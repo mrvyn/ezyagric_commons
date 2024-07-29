3a8f541051af9f6dfc3fa6241962494caf846636
Mattie Mark <marvinbusuulwa@gmail.com>
2024-07-29 16:11:13 +0300

    V3 code

---
+++ lib/src/dashboard.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+import '../ezyagric_commons.dart';
+import 'styles/colors.dart';
+
+class DashboardPage extends StatefulWidget {
+  DashboardPage(
+      {super.key,
+      required this.homePage,
+      required this.ordersPage,
+      required this.profilePage});
+  Widget homePage;
+  Widget ordersPage;
+  Widget profilePage;
+
+  // var screens;
+
+  @override
+  State<DashboardPage> createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    var screens = [
+      widget.homePage,
+      widget.ordersPage,
+      widget.profilePage,
+      Container()
+    ];
+    return Scaffold(
+      body: screens[currentIndex], //getNavigatorItems()[currentIndex].screen,
+      backgroundColor: Colors.white,
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black38.withOpacity(0.1),
+                spreadRadius: 0,
+                blurRadius: 37,
+                offset: Offset(0, -12)),
+          ],
+        ),
+        child: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            // Fixed
+            // backgroundColor: const Color(0xFF033860),
+            // <-- This works for fixed
+            // selectedItemColor: Colors.grey,
+            // unselectedItemColor: Colors.blueGrey,
+
+            selectedItemColor: AppColors.primaryColor,
+            selectedLabelStyle: TextStyle(fontWeight: FontWeight.w500),
+            unselectedLabelStyle: TextStyle(fontWeight: FontWeight.w400),
+            unselectedItemColor: Colors.blueGrey,
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.shopping_bag_outlined),
+                label: 'My orders',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: 'Profile',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.more_horiz),
+                label: 'More',
+              ),
+            ],
+            currentIndex: currentIndex,
+            onTap: (int index) {
+              if (index == 3) {
+                showMoreBottomSheet();
+              } else {
+                setState(() {
+                  currentIndex = index;
+                });
+              }
+            }),
+      ),
+    );
+  }
+}
---
+++ lib/src/styles/colors.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  //One instance, needs factory
+  static AppColors? _instance;
+  factory AppColors() => _instance ??= AppColors._();
+
+  AppColors._();
+
+  static const primaryColor = Color(0xFF018BC9);
+  static const primaryColorDark = Color(0xFF1D4465);
+  static const darkGrey = Color(0xff7C7C7C);
+  static const whiteColor = Color(0xffffffff);
+  static const secondaryColor = Color(0xFF589165);
+  static const secondaryColorDark = Color(0xFF327A43);
+  static const success = Color(0xFF27AE60);
+  static Color blackColor = Colors.black;
+  static Color greyText = Colors.grey.shade700;
+  static const lightGreen = Color(0xffe0f5e4);
+  static const lightColor = Color(0xffebeded);
+  static const cardBackgroundColor = Color(0xfff3f4f4);
+  static const lighterGray = Color(0xFFF2F3F2);
+  static const darkerGray = Color(0xFF777474);
+  static const color1 = Color(0xFFC4C4C4);
+  static const color2 = Color(0xffe65100);
+  static const color3 = Color(0xff3F7BB6);
+  static const color4 = Color(0xffBDBDBD);
+  static const color5 = Color(0xff1F1C1E);
+  static const color6 = Color(0xff292929);
+  static const color7 = Color(0xff141E26);
+  static const color8 = Color(0xff1F4564);
+  static const color9 = Color(0xff27AE60);
+  static const color10 = Color(0xffF5F5F5);
+  static const color11 = Color(0xff018BC9);
+  static const color12 = Color(0xffE1572C);
+  static const color13 = Color(0xff255EB2);
+  static const color14 = Color(0xff1D1F21);
+  static const color15 = Color(0xff01B235);
+  static const color16 = Color(0xff707070);
+  static const color17 = Color(0xffF1A104);
+  static const color18 = Color(0xffC7DFF6);
+  static const color19 = Color(0xff149078);
+  static const color20 = Color(0xff6F7275);
+  static const color21 = Color(0xff3C7AB8);
+  static const color22 = Color(0xffDCDDDD);
+  static const color23 = Color(0xffA9ABAB);
+  static const color24 = Color(0xff828282);
+  static const color25 = Color(0xffB13D3D);
+  static const color26 = Color(0xff838383);
+  static const color27 = Color(0xffD2F2D9);
+  static const color28 = Color(0xffF9A31C);
+  static const color29 = Color(0xffFC6F1F);
+  static const color30 = Color(0xff4CAB94);
+  static const color31 = Color(0xff05377E);
+  static const color32 = Color(0xffE7EDF0);
+  static const color33 = Color(0xffFFEFD1);
+  static const color34 = Color(0xffBB7F09);
+  static const color35 = Color(0xff6563FF);
+  static const color36 = Color(0xffD9D9D9);
+  static const color37 = Color(0xffD74533);
+}
